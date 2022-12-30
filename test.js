2c5217188ff237b18ef42c282e20fa450fc40451
lm4183-0 <121673639+lm4183-0@users.noreply.github.com>
2022-12-30 23:29:58 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,12 @@
-<script type="text/javascript">
-
 var obj = new XMLHttpRequest();
 var str = "";
 
 obj.open('GET','https://raw.githubusercontent.com/lm4183-0/test/main/test.csv',true);
 obj.onreadystatechange = function(){
-  if (obj.readyState === 4 && obj.status === 200){
-    str = obj.responseText;
+	if (obj.readyState === 4 && obj.status === 200){
+		str = obj.responseText;
 	}
 };
-
 obj.send(null);
+
+document.getElementById("debug").innerHTML = str2;
