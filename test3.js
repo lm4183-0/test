198e026ff1e96187b59ff82b41d91e093fb6a6b7
lm4183-0 <121673639+lm4183-0@users.noreply.github.com>
2022-12-30 23:41:14 +0900

    Update test3.js

--- test3.js
+++ test3.js
@@ -1,3 +1,7 @@
-<script type="text/javascript">
-  document.getElementById("debug").innerHTML = "test3";
-</script>
+function exec() {
+    var elem = document.getElementById("output");
+    var now = new Date();
+    var hour = now.getHours();
+    var min = now.getMinutes();
+    elem.innerHTML = "ボタンがクリックされました。 (" + hour +":" + min + ")";
+}
